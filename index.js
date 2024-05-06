01ab96cdfb7df23cb14e028a55f11b9a6a051dc0
Ruben Otero <ruben87sp@gmail.com>
2024-05-06 20:55:19 +0200

    still does not work

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const pool = require('./client.js');
 const port = process.env.PORT;
 const usersRouter = require('./routes/users_routes');
 const cors = require('cors');
+const { authenticateToken } = require('./middleware/authorization.js');
 
 // Enable CORS for all origins
 app.use(cors());
@@ -12,6 +13,12 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Homepage');
+});
+
+app.get('/secure-route', authenticateToken, (req, res) => {
+    // This route is protected by authentication middleware
+    // Only authenticated users can access it
+    res.send('Welcome to the secure route');
 });
 
 app.use('/users', usersRouter); // Mount usersRouter at '/users' base path
--- routes/auth_routes.js
+++ routes/auth_routes.js
@@ -9,22 +9,20 @@ const router = express.Router();
 router.post('/login', async (req, res) => {
     try {
         const { email, password } = req.body;
-        const users = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
-        if(users.rows.length === 0) return res.status(401).json({error : "Email is incorrect"});
+        const user = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+        if (user.rows.length === 0) return res.status(401).json({ error: "Email is incorrect" });
 
-        const validPassword = await bcrypt.compare(password, users.rows[0].password);
-        if(!validPassword) return  res.status(401).json({error : "Password is incorrect"});
+        const isValidPassword = await bcrypt.compare(password, user.rows[0].password);
+        if (!isValidPassword) return res.status(401).json({ error: "Password is incorrect" });
 
-        let tokens = jwtTokens(users.rows[0]);
-        res.cookie('refresh_token', tokens.refreshToken, {
-            httpOnly: true
-        });
-        res.json(tokens);
-
+        // Generate JWT token
+        const accessToken = jwt.sign({ userId: user.rows[0].id }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '15m' });
+        res.json({ accessToken });
     } catch (error) {
-        res.status(401).json({error : error.message});
+        res.status(500).json({ error: error.message });
     }
 });
+
 
 router.get('/refresh_token', (req, res) => {
     try {
