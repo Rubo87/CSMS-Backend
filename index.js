15b2e843a48b872146474fb4385111205bd552c2
Ruben Otero <ruben87sp@gmail.com>
2024-05-12 10:03:48 +0200

    edit and put methods added to API

--- index.js
+++ index.js
@@ -34,6 +34,40 @@ app.get('/api/language-schools', async (req, res) => {
     }
 });
 
+app.delete('/api/language-schools/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+        // Perform deletion operation here using the id
+        // For example, you can execute a DELETE query in your database
+        await pool.query('DELETE FROM language_schools WHERE id = $1', [id]);
+        res.status(204).send(); // No content, successful deletion
+    } catch (err) {
+        console.error('Error executing delete query', err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+app.put('/api/language-schools/:id', async (req, res) => {
+    const id = req.params.id;
+    const updatedData = req.body;
+    try {
+        const { classValue, companyName, users, firstName, lastName, city, country } = updatedData;
+        const result = await pool.query(
+            'UPDATE language_schools SET class = $1, companyName = $2, users = $3, firstName = $4, lastName = $5, city = $6, country = $7 WHERE id = $8',
+            [classValue, companyName, users, firstName, lastName, city, country, id]
+        );
+        if (result.rowCount === 1) {
+            res.status(200).json({ message: 'Data updated successfully' });
+        } else {
+            res.status(404).json({ error: 'Data not found' });
+        }
+    } catch (err) {
+        console.error('Error updating data:', err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
