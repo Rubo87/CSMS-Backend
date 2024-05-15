cb34022441145e22fa80e63008e7d24b0bb993cd
Ruben Otero <ruben87sp@gmail.com>
2024-05-15 21:20:06 +0200

    get event types

--- index.js
+++ index.js
@@ -148,19 +148,6 @@ app.post('/api/calendar-events', async (req, res) => {
     }
 });
 
-app.post('/', async (req, res) => {
-    try {
-        const hashedPassword = await bcrypt.hash(req.body.password, 10);
-        const newUser = await pool.query('INSERT INTO users (username, email, password, name, surname, role) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *', [req.body.username, req.body.email, hashedPassword, req.body.name, req.body.surname, req.body.role]);
-
-        res.json({
-            user: newUser.rows[0]
-        });
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-});
-
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
